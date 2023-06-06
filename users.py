2957d428c82d006e426e9305e94fc7aa8d3f18bf
Aaron <aaron.hsu86+github@gmail.com>
2023-06-06 13:38:39 -0700

    updated display results

--- users.py
+++ users.py
@@ -9,27 +9,29 @@ class User:
             "savings" : BankAccount(0.01, 0)
         }
     
-    # other methods
     # make deposit into account
     def make_deposit(self, account_name, amount):
-        if valid_account(account_name, self):
+        if self.valid_account(account_name, self):
             self.accounts[account_name].deposit(amount)
         return self
     # make withdrawal from account
     def make_withdrawal(self, account_name, amount):
-        if valid_account(account_name, self):
+        if self.valid_account(account_name, self):
             self.accounts[account_name].withdraw(amount)
         return self
     # display account balance
-    def display_user_balance(self, account_name):
-        if valid_account(account_name, self):
-            print(f'{account_name.capitalize()}')
-            self.accounts[account_name].display_account_info()
+    def display_user_balance(self):
+        print(f'{self.name}\'s Bank Accounts')
+        # display balance for all user's accounts
+        for acc in self.accounts:
+            print(f'{acc.capitalize()} Account')
+            self.accounts[acc].display_account_info()
+
         return self
     # transfer from one account to another
     def transfer_money(self, account_name, amount, other_user, other_account):
         # check if both users have valid accounts
-        if valid_account(account_name, self) and valid_account(other_account, other_user):
+        if self.valid_account(account_name, self) and other_user.valid_account(other_account, other_user):
             # attempt withdrawal
             self.accounts[account_name].withdraw(amount)
             # only proceed with giving other account money if user has enough
@@ -39,10 +41,11 @@ class User:
             print('Please check account names')
         return self
 
-# Create function to check if account name is valid
-def valid_account(account_name, self):
-    if account_name == 'savings' or account_name == 'checking':
-        return True
-    else:
-        print(f'{self.name} does not have a {account_name} account.\nPlease check spelling.')
-        return False
+    @staticmethod
+    # Function to check if account name is valid
+    def valid_account(account_name, self):
+        if account_name == 'savings' or account_name == 'checking':
+            return True
+        else:
+            print(f'{self.name} does not have a {account_name} account.\nPlease check spelling.')
+            return False
